1cfc10d3a2bc5fecbad8c077dbd5b2019b53b32a
Leandro Guarido <leandro.guarido@gmail.com>
2022-07-12 17:53:57 -0300

    rest testado

---
+++ src/main/java/com/curso/java/resources/CategoriaResource.java
@@ -0,0 +1,17 @@
+package com.curso.java.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+	return "rest esta funfando";
+	
+	}
+	
+}
