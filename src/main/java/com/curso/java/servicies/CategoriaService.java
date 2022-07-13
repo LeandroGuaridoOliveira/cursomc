324a8fa5301016f70499f2ad36e56c375c205786
Leandro Guarido <leandro.guarido@gmail.com>
2022-07-13 19:08:08 -0300

    ajustes endPoint /categorias / id

--- src/main/java/com/curso/java/servicies/CategoriaService.java
+++ src/main/java/com/curso/java/servicies/CategoriaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 import com.curso.java.repositories.CategoriaRepository;
+import com.curso.java.servicies.exceptions.ObjectNotFoundExecption;
 import com.curso.java.domain.Categoria;
 
 @Service
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
+		 if(obj == null) {
+			 throw new ObjectNotFoundExecption("Objeto nao encontrado. id: " + id
+		+ ", Tipo: " + Categoria.class.getName());			 
+		 }
 		return obj.orElse(null);
 		} 
 
